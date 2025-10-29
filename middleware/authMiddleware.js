1b6d44ad56d255aee710dc9754d634b8f59b165d
AnietieDaniel <anietienteabasi@gmail.com>
2025-10-29 10:04:34 +0100

    Thank You Lord

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,17 +1,23 @@
 import jwt from 'jsonwebtoken';
 
 export const protect = (req, res, next) => {
-  const token = req.cookies.token; // ✅ get from cookie
-  console.log("decoded token: ", req.cookies)
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];; // get JWT from cookie
+
+  console.log("🔹 Token from cookie:", token); // <- log raw token
+
   if (!token) {
     return res.status(401).json({ message: 'No token provided' });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // or fetch from DB if needed
+    console.log("🔹 Decoded token payload:", decoded); // <- log decoded payload
+
+    req.user = decoded; // attach to request
+    req.token = token;  // attach raw token if needed in controller
     next();
   } catch (err) {
+    console.error("❌ JWT verification error:", err.message);
     return res.status(401).json({ message: 'Invalid or expired token' });
   }
 };
